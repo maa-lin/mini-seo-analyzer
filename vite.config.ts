a73b54dae9c09d3e5aad7e2ac7ed1cdbf611954d
Malin <maa.lin@outlook.com>
2026-01-13 12:03:58 +0100

    Remove quotes from base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  "base": "/mini-seo-analyzer/",
+  base: "/mini-seo-analyzer/",
   plugins: [react()],
 })
